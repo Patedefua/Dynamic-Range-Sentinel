40bc43c4eec7414c96c8a146ff848e9e4d3d14b7
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-18 19:59:33 -0300

    Create PluginProcessor.h

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+
+class DynamicRangeSentinelProcessor : public juce::AudioProcessor
+{
+public:
+    DynamicRangeSentinelProcessor();
+    ~DynamicRangeSentinelProcessor() override;
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    const juce::String getName() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    double getTailLengthSeconds() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void changeProgramName(int index, const juce::String& newName) override;
+
+    void getStateInformation(juce::MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+
+private:
+    juce::AudioBuffer<float> lookaheadBuffer;
+    int writePosition = 0;
+    double lastSampleRate = 44100.0;
+    float smoothedGain = 1.0f;
+
+    std::atomic<float>* targetPeakParam = nullptr;
+    std::atomic<float>* lookaheadParam = nullptr;
+
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DynamicRangeSentinelProcessor)
+};

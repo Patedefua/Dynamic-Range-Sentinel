4e2f002a68ec90adbced4b86d3ca7b46e3fb5725
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-18 20:00:35 -0300

    Create PluginEditor.cpp

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,33 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+DynamicRangeSentinelEditor::DynamicRangeSentinelEditor (DynamicRangeSentinelProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    backgroundImage = juce::ImageCache::getFromMemory(BinaryData::background_png, BinaryData::background_pngSize);
+
+    targetPeakKnob.setSliderStyle(juce::Slider::RotaryVerticalDrag);
+    targetPeakKnob.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(targetPeakKnob);
+    targetPeakAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "TARGET_PEAK", targetPeakKnob);
+
+    lookaheadSlider.setSliderStyle(juce::Slider::LinearHorizontal);
+    lookaheadSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(lookaheadSlider);
+    lookaheadAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "LOOKAHEAD", lookaheadSlider);
+
+    setSize (700, 450);
+}
+
+DynamicRangeSentinelEditor::~DynamicRangeSentinelEditor() {}
+
+void DynamicRangeSentinelEditor::paint (juce::Graphics& g)
+{
+    g.drawImage(backgroundImage, getLocalBounds().toFloat());
+}
+
+void DynamicRangeSentinelEditor::resized()
+{
+    targetPeakKnob.setBounds(95, 175, 150, 150);
+    lookaheadSlider.setBounds(90, 395, 520, 25);
+}

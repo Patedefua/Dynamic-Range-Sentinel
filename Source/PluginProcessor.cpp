b3562ae8e9ccfabb213be38f5f791f30801ef6d1
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-19 03:35:54 -0300

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,4 +1,6 @@
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_gui_extra/juce_gui_extra.h>
 #include "PluginProcessor.h"
 
 DynamicRangeSentinelProcessor::DynamicRangeSentinelProcessor()

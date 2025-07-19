5bcd70aae8522b6652212c9a30b2f0b2f9b345c7
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-19 18:56:20 -0300

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,9 +14,16 @@ public:
 
 private:
     DynamicRangeSentinelAudioProcessor& processorRef;
+    
+    // ⚡ INTEGRACIÓN GRÁFICA: Variable para guardar la imagen de fondo.
+    juce::Image backgroundImage;
 
     juce::Slider thresholdSlider, ceilingSlider;
     juce::Label thresholdLabel, ceilingLabel;
+
+    // ⚡ CONEXIÓN AUTOMATIZADA: Los "attachments" conectan los sliders a los parámetros automáticamente.
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> thresholdAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> ceilingAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DynamicRangeSentinelEditor)
 };

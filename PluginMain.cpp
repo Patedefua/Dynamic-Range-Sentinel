446a2a4a5c0b2140e3f5a924791fbf207184a086
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-19 18:50:08 -0300

    Delete PluginMain.cpp

--- PluginMain.cpp
+++
@@ -1,6 +0,0 @@
-#include "PluginProcessor.h"
-
-// Esta función se compila dentro del target del plugin (no en SharedCode)
-const juce::String DynamicRangeSentinelProcessor::getName() const {
-    return JucePlugin_Name;
-}

f013d88a254e916102721260ac086987eab4dd85
Patedefua <159368604+Patedefua@users.noreply.github.com>
2025-07-19 02:00:40 -0300

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,6 +2,8 @@
 
 #include "PluginProcessor.h"
 
+#if JUCE_BUILD_GUI
+
 class DynamicRangeSentinelEditor  : public juce::AudioProcessorEditor
 {
 public:
@@ -13,13 +15,15 @@ public:
 
 private:
     DynamicRangeSentinelProcessor& audioProcessor;
-    
+
     juce::Image backgroundImage;
     juce::Slider targetPeakKnob;
     juce::Slider lookaheadSlider;
 
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> targetPeakAttachment;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> lookaheadAttachment;
-    
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DynamicRangeSentinelEditor)
 };
+
+#endif // JUCE_BUILD_GUI
